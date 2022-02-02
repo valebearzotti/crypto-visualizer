1b24302558b31f571e9af0e3baefed6f0bfb9ad0
Valentina Bearzotti <valebearzotti1@gmail.com>
2022-02-02 17:58:09 -0300

    first setup + first try at crypto api (works well but does nothing but showing highest value only)

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import axios from 'axios'
+import styled from 'styled-components'
+
+function App() {
+
+  
+
+  //  Each bucket is an array of the following information:
+
+  //  time bucket start time
+  //  low lowest price during the bucket interval
+  //  high highest price during the bucket interval
+  //  open opening price (first trade) in the bucket interval
+  //  close closing price (last trade) in the bucket interval
+  //  volume volume of trading activity during the bucket interval
+
+  
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
